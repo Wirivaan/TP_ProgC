e85a5696a0af853ae49c6973538ad560894db385
Angie Rios <117636524+Wirivaan@users.noreply.github.com>
2025-12-03 21:08:13 +0100

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+int a = 16;
+
+int b = 3;
+
+
+printf("a + b = %d\n", a+b );
+
+printf("a - b = %d\n", a-b );
+
+printf("a * b = %d\n", a*b );
+
+printf("a / b = %d\n", a/b);
+
+printf("a modulo b = %d \n", a%b );
+  
+printf("a == b: %d\n", a == b);
+  
+printf("a > b: %d\n", a > b);
+  
+
+}
