1ea55c66db531d1d6debee4cb1e8feda18629891
Angie Rios <117636524+Wirivaan@users.noreply.github.com>
2025-12-03 19:41:27 +0100

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int rayon() {
 double pi = 3.1416;
 double r = 6;  
     printf("L'air du cercle = %f", pi*(r*r));
    printf("Le périmètre du cercle = %f", pi*(r+r));
 }
+
 
