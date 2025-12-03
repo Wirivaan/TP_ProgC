41edfcd6b03286c1aaf0ec11a622a96e4994f9a6
Angie Rios <117636524+Wirivaan@users.noreply.github.com>
2025-12-03 19:57:36 +0100

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
 
-  int typeint = 5;
-  
+    printf("char:                 %lu octets\n", sizeof(char));
+    printf("signed char:          %lu octets\n", sizeof(signed char));
+    printf("unsigned char:        %lu octets\n", sizeof(unsigned char));
+
+    printf("short:                %lu octets\n", sizeof(short));
+    printf("signed short:         %lu octets\n", sizeof(signed short));
+    printf("unsigned short:       %lu octets\n", sizeof(unsigned short));
+
+    printf("int:                  %lu octets\n", sizeof(int));
+    printf("signed int:           %lu octets\n", sizeof(signed int));
+    printf("unsigned int:         %lu octets\n", sizeof(unsigned int));
+
+    printf("long int:             %lu octets\n", sizeof(long int));
+    printf("signed long int:      %lu octets\n", sizeof(signed long int));
+    printf("unsigned long int:    %lu octets\n", sizeof(unsigned long int));
+
+    printf("long long int:        %lu octets\n", sizeof(long long int));
+    printf("signed long long int: %lu octets\n", sizeof(signed long long int));
+    printf("unsigned long long:   %lu octets\n", sizeof(unsigned long long int));
+
+    printf("float:                %lu octets\n", sizeof(float));
+    printf("double:               %lu octets\n", sizeof(double));
+    printf("long double:          %lu octets\n", sizeof(long double));
+
+    return 0;
 }
+
 
